555876f439c3fb48c01d9a2beb7d1a79c8820747
Michael Bonar <michael.w.bonar@gmail.com>
2019-12-30 11:56:05 -0800

    linting is a bloody mess in vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,16 @@ module.exports = {
   env: {
     node: true
   },
+  plugins: ["es-beautifier"],
   extends: [
-    'plugin:vue/recommended',
-    'plugin:prettier/recommended',
-    '@vue/prettier'
+    'plugin:vue/essential',
+    'plugin:es-beautifier/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ["error", "never"],
+    'linebreak-style': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
