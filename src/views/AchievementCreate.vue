e0202e2bee5348cc20d6a8c91436c6ddcf883add
Michael Bonar <michael.w.bonar@gmail.com>
2019-12-30 13:45:02 -0800

    changed event to achievment

--- src/views/AchievementCreate.vue
+++ src/views/AchievementCreate.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Create an event</h1>
+    <h1>Create an achievement</h1>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style></style>
